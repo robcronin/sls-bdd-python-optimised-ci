4fa8271c180f9f8d7a86d92d9af194b2714aa2a7
robcronin <robc@theodo.co.uk>
2019-11-20 23:10:15 +0000

    Add linter

--- features/steps/calls.py
+++ features/steps/calls.py
@@ -2,22 +2,26 @@ from behave import given, when, then, step
 import requests
 import json
 
+
 @given('I call the "{endpoint}" endpoint')
 def call_endpoint(context, endpoint):
     response = requests.get(f'{context.env.get("domain_name")}{endpoint}')
     context.last_response = response
+
 
 @then('I see "{message}" in the "{key}" of the response')
 def see_string_in_response(context, message, key):
     body = json.loads(context.last_response.text)
     assert body[key] == message
 
+
 @then('I see {message:d} in the "{key}" of the response')
 def see_integer_in_response(context, message, key):
     body = json.loads(context.last_response.text)
     assert body[key] == message
 
-@then('I get a {status:d} response')
+
+@then("I get a {status:d} response")
 def see_status_in_response(context, status):
     print("context", context.last_response.status_code, "\n")
     assert context.last_response.status_code == status
--- src/hello.py
+++ src/hello.py
@@ -1,5 +1,6 @@
 from src.response import make_response
 
+
 def hello(event, context):
     body = {
         "message": "Go Serverless v1.0! Your function executed successfully in python!",
